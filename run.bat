76c56d89be78e45cbf1610cc4b4ef9b6182ff9ca
zadolbali10 <anon.yu@mail.ru>
2022-10-02 14:22:26 +0300

    Added run file

---
+++ run.bat
@@ -0,0 +1,11 @@
+del res.txt
+echo 1 >> res.txt
+main 600 800 1000 1 >> res.txt
+echo 2 >> res.txt
+main 600 800 1000 2 >> res.txt
+echo 4 >> res.txt
+main 600 800 1000 4 >> res.txt
+echo 8 >> res.txt
+main 600 800 1000 8 >> res.txt
+echo 16 >> res.txt
+main 600 800 1000 16 >> res.txt
